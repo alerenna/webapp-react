7b8b3cc441dd8d2e91d230be6d7bcac865e88210
“alerenna” <alessandrorennafm@gmail.com>
2025-04-16 18:37:42 +0800

    Add movie retrieval to Homepage and update layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from "react"
+
 export default function Hompepage() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3005/api/v1/movies')
+
+            .then(res => res.json())
+            .then(data => {
+                setMovies(data)
+            })
+            .catch(err => {
+                console.log(err);
+
+            })
+    }, [])
 
     return (
         <>
-            <h1>HOMEPAGE</h1>
+            <div className="container">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
+
+
     )
 }
